a70c894b323c659060d0ac978d043a7d80b236ad
Igor <iigorfelipe@gmail.com>
2024-10-22 09:53:09 -0300

    feat: implements works section

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from '@/components/theme-provider';
 import { Home } from './sections/home';
 import { About } from './sections/about';
 import { Header } from './components/header';
+import { Works } from './sections/works';
 
 function Divider() {
   return <div className="border-b w-full" />;
@@ -10,14 +11,22 @@ function Divider() {
 export const App = () => {
   return (
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <Header />
       <main className="flex flex-col">
+      <Header />
         <section className="h-screen w-full pt-12">
           <Home />
         </section>
+
         <Divider />
+
         <section className="h-screen w-full pt-12 px-8">
           <About />
+        </section>
+
+        <Divider />
+
+        <section className="h-screen w-full px-4">
+          <Works />
         </section>
       </main>
     </ThemeProvider>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,12 +3,12 @@ import { Themes } from './theme-provider';
 
 export function Header() {
   return (
-    <header className="flex justify-between fixed top-0 left-0 w-full rounded-lg backdrop-blur-md">
-      <div className="sm-laptop:hidden rounded-lg z-10">
+    <header className="flex justify-between fixed top-0 border-b left-0 w-full backdrop-blur-md z-10">
+      <div className="sm-laptop:hidden rounded-lg">
         <SideBar />
       </div>
 
-      <div className="ml-auto hidden sm-laptop:flex rounded-lg z-10">
+      <div className="ml-auto hidden sm-laptop:flex rounded-lg">
         <Themes />
       </div>
     </header>
