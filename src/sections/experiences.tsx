f9588e5ca0a2a58f2940973623cbe6144d86a665
Igor <iigorfelipe@gmail.com>
2024-10-25 14:19:50 -0300

    feat: add modal to display experience details

--- src/sections/experiences.tsx
+++ src/sections/experiences.tsx
@@ -7,45 +7,61 @@ import {
   CarouselPrevious,
 } from '@/components/ui/carousel';
 import { useMediaQuery } from '@/hooks/use-media-query';
-import { BriefcaseBusinessIcon, CircleEllipsisIcon } from 'lucide-react';
+import { BriefcaseBusinessIcon, EllipsisIcon } from 'lucide-react';
 import { useEffect, useState } from 'react';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { useIntersectionObserver } from '@/hooks/use-intersection-observer';
+import { TooltipDemo } from '@/components/tooltip';
 
 type Log = {
   date: string;
   position: string;
 };
 
+type CompanyDetail = {
+  companyName: string;
+  color: string;
+  about: string[];
+  duration: string;
+  logo: string;
+  roleDetails: string[];
+  log: Log[];
+};
+
 type Experiences = {
-  trybeExperience: {
-    company: string;
-    duration: string;
-    logo: string;
-    roleDetails: string[];
-    log: Log[];
-  };
-  atlasExperience: {
-    company: string;
-    duration: string;
-    logo: string;
-    roleDetails: string[];
-    log: Log[];
-  };
+  trybeExperience: CompanyDetail;
+  atlasExperience: CompanyDetail;
 };
 
 const experiences: Experiences = {
   trybeExperience: {
-    company: 'Trybe - Escola de Tecnologia online',
+    companyName: 'Trybe - Escola de Tecnologia online',
+    color: '#1EB720',
+    about: [
+      `A Trybe é uma escola de tecnologia online fundada em 2019, que oferece um currículo prático, desenvolvido em parceria com empresas do setor. A abordagem da Trybe combina teoria e prática, preparando os alunos com habilidades técnicas essenciais, como desenvolvimento web, análise de dados e ciência da computação.`,
+
+      `Além do conhecimento técnico, a Trybe forma profissionais competentes e prontos para o mercado, enfatizando competências comportamentais como trabalho em equipe, resolução de problemas e adaptabilidade. Com um compromisso com a inclusão e diversidade, a Trybe conecta talentos a oportunidades valiosas, contribuindo para o fortalecimento da indústria de tecnologia.`,
+    ],
     duration: 'ago de 2021 | ago de 2022',
     logo: 'trybe-logo.svg',
     roleDetails: [
-      'Desenvolvimento e manutenção de aplicações web dinâmicas e intuitivas usando Next.js, React, TypeScript, Context API, Redux, Material UI.',
-      'Colaboração com designer utilizando Figma para criação de interfaces de usuário intuitivas e responsivas.',
-      'Criação de componentes performáticos.',
-      'Documentação das minhas atividades.',
-      'Atualizações de tabelas no MongoDB e MySQL.',
-      'Criação de microsserviços para automatizar tarefas usando Node.js.',
-      'Reuniões diárias sobre o progresso das atividades.',
+      'Aprofundamento nos fundamentos do desenvolvimento web, incluindo Git, Unix, Bash, HTML, CSS e JavaScript.',
+      'Desenvolvimento de aplicações frontend utilizando TypeScript, React, React Hooks e React Router, com ênfase em acessibilidade e usabilidade.',
+      'Implementação de testes unitários e automação de testes com React Testing Library, garantindo a qualidade e robustez do código.',
+      'Desenvolvimento de backend em Node.js, incluindo Programação Orientada a Objetos, construção de APIs REST com Express.js e gerenciamento de banco de dados MySQL utilizando Sequelize.',
+      'Utilização de Docker para contêinerização de aplicações, promovendo um ambiente de desenvolvimento eficiente.',
+      'Participação ativa em projetos em grupo, praticando habilidades interpessoais como pensamento crítico, colaboração e comunicação assertiva.',
+      'Participação em workshops e atividades de soft skills, promovendo liderança e trabalho em equipe em um ambiente dinâmico e desafiador.',
     ],
+
     log: [
       {
         date: 'ago de 2021',
@@ -71,17 +87,30 @@ const experiences: Experiences = {
   },
 
   atlasExperience: {
-    company: 'Atlas - Segurança e Inteligência Logística',
+    companyName: 'Atlas - Segurança e Inteligência Logística',
+    color: '#FF5618',
     duration: 'out de 2022 | jun de 2024',
     logo: 'atlas-logo.svg',
+    about: [
+      `A Atlas atua no mercado de Segurança e Inteligência Logística, reconhecida pelo seu trabalho em
+        proteger operações logísticas por meio de soluções tecnológicas de ponta. Atuando desde 2004, a
+        empresa se destaca por atender centenas de clientes de diversos setores e tamanhos, oferecendo um alto
+        nível de segurança e controle para suas operações.`,
+
+      `Com um compromisso sólido de qualidade, a Atlas adota uma abordagem personalizada, ajustando suas
+        soluções às necessidades específicas de cada cliente. Esse foco em inovação e atendimento diferenciado
+        a posiciona como uma referência no mercado, com parcerias de longo prazo e uma reputação de
+        excelência. Trabalhar na Atlas significa atuar em uma equipe qualificada e apaixonada, empenhada em
+        entregar o melhor em segurança e tecnologia logística.`,
+    ],
     roleDetails: [
-      'Desenvolvimento e manutenção de aplicações web dinâmicas e intuitivas usando Next.js, React, TypeScript, Context API, Redux, Material UI.',
-      'Colaboração com designer utilizando Figma para criação de interfaces de usuário intuitivas e responsivas.',
-      'Criação de componentes performáticos.',
-      'Documentação das minhas atividades.',
-      'Atualizações de tabelas no MongoDB e MySQL.',
-      'Criação de microsserviços para automatizar tarefas usando Node.js.',
-      'Reuniões diárias sobre o progresso das atividades.',
+      'Desenvolvi e mantive aplicações web modernas e dinâmicas, utilizando Next.js, React, TypeScript, Context API, Redux e Material UI para garantir interfaces intuitivas e responsivas.',
+      'Colaborei com designers no Figma para criar experiências de usuário otimizadas, alinhando design e funcionalidade.',
+      'Desenvolvi componentes performáticos, priorizando eficiência e reutilização para otimizar a experiência do usuário e o desempenho da aplicação.',
+      'Documentei minhas atividades de desenvolvimento, mantendo registros claros para facilitar manutenções futuras e o alinhamento da equipe.',
+      'Realizei atualizações e manipulação de dados em bancos de dados como MongoDB e MySQL, garantindo integridade e performance dos dados.',
+      'Implementei microsserviços em Node.js para automatizar tarefas, aprimorando processos internos e aumentando a produtividade da equipe.',
+      'Participei de reuniões diárias de alinhamento, reportando o progresso das atividades e colaborando para o sucesso coletivo do projeto.',
     ],
     log: [
       {
@@ -96,8 +125,10 @@ const experiences: Experiences = {
   },
 };
 
+type ComapnyIdType = 'trybeExperience' | 'atlasExperience';
+
 type ExperienceType = Log & {
-  companyId: 'trybeExperience' | 'atlasExperience';
+  companyId: ComapnyIdType;
 };
 
 const page1: ExperienceType[] = [
@@ -144,14 +175,70 @@ const page2: ExperienceType[] = [
 type CardProps = {
   experience: ExperienceType;
 };
+type DialogDemoProps = {
+  companyId: ComapnyIdType;
+};
+
+export function DialogDemo({ companyId }: DialogDemoProps) {
+  const [togleCard, setTogleCard] = useState(false);
+
+  const { companyName, color, roleDetails, about } = experiences[companyId];
+
+  const content = togleCard ? (
+    <ul className="list-disc px-6">
+      {roleDetails.map((detail, index) => (
+        <li key={index} className="py-2">
+          {detail}
+        </li>
+      ))}
+    </ul>
+  ) : (
+    about.map((detail, index) => (
+      <p key={index} className="px-6">
+        {detail}
+      </p>
+    ))
+  );
+
+  const subTitle = companyName.includes('Trybe')
+    ? 'Meus aprendizados na Trybe'
+    : `Meu Trabalho na ${companyName.split(' - ')[0]}`;
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="ghost" size="icon" className="flex size-6">
+          <TooltipDemo
+            children={<EllipsisIcon strokeWidth={1} className="laptop:size-5" />}
+            tooltip="Exibir detalhes"
+          />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-md overflow-y-auto max-h-full rounded-xl">
+        <DialogHeader>
+          <DialogTitle style={{ color: color }} className="font-bold">
+            {companyName}
+          </DialogTitle>
+          <DialogDescription>{togleCard ? subTitle : 'Sobre a Empresa'}</DialogDescription>
+        </DialogHeader>
+        {content}
+        <DialogFooter className="sm:justify-start">
+          <Button type="button" variant="secondary" onClick={() => setTogleCard(!togleCard)}>
+            {togleCard ? 'Ver Sobre a Empresa' : `Ver ${subTitle}`}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
 
 function Card({ experience }: CardProps) {
   return (
     <div className="flex items-center gap-4 w-full max-w-[400px]">
-      <picture className="size-16 sm-laptop:size-[50px] 70-laptop:size-[70px] laptop:size-[76px] xs-desktop:size-20 70-desktop:size-24 2xl-desktop:size-28">
+      <picture className="size-16 sm-laptop:size-[50px] 70-laptop:size-[70px] laptop:size-[76px] xs-desktop:size-[78px]">
         <img
           src={experiences[experience.companyId].logo}
-          alt={`Logo of ${experiences[experience.companyId].company}`}
+          alt={`Logo of ${experiences[experience.companyId].companyName}`}
           className="w-full h-full drop-shadow-md"
         />
       </picture>
@@ -164,25 +251,21 @@ function Card({ experience }: CardProps) {
           {experience.position}
         </span>
 
-        <Button variant="link" className="p-0 pr-8 flex gap-2 w-full items-center -mt-[9px] laptop:-mt-1">
-          <span className="text-xs sm-laptop:text-[11px] 70-laptop:text-[13px] laptop:text-base xs-desktop:text-sm 70-desktop:text-base">
-            Detalhes
-          </span>
-          <CircleEllipsisIcon strokeWidth={1} className="size-4 ml-auto laptop:size-5" />
-        </Button>
+        <DialogDemo companyId={experience.companyId} />
       </div>
     </div>
   );
 }
 export function Experiences() {
+  const { ref, isIntersecting } = useIntersectionObserver(0.4);
+  const [groupedExperiences, setGroupedExperiences] = useState<ExperienceType[][]>([page1, page2]);
+
   const isMobile = useMediaQuery('(max-width: 499px)');
   const widthIs896pxDown = useMediaQuery('(max-width: 896px)');
   const imgLaptopSize =
     'sm-laptop:max-w-60 md-laptop:max-w-xs md-laptop:-mt-8 70-laptop:mt-auto 70-laptop:-ml-60 70-laptop:max-w-sm';
   const imgDesktopSize =
     'xs-desktop:mt-0 xs-desktop:max-w-xs sm-desktop:max-w-md md-desktop:max-w-lg md-desktop:mt-auto md-desktop:mr-0 70-desktop:max-w-xl';
-
-  const [groupedExperiences, setGroupedExperiences] = useState<ExperienceType[][]>([page1, page2]);
 
   useEffect(() => {
     if (!widthIs896pxDown) {
@@ -219,7 +302,17 @@ export function Experiences() {
       )}
 
       {!widthIs896pxDown && (
-        <div className="flex 70-laptop:justify-start 70-laptop:gap-12 70-laptop:w-full -mr-32 70-desktop:justify-start w-full">
+        <div
+          className="flex 70-laptop:justify-start 70-laptop:gap-12 70-laptop:w-full -mr-32 70-desktop:justify-start w-full"
+          ref={ref}
+          style={{
+            opacity: isIntersecting ? 1 : 0,
+            transform: isIntersecting ? 'translateX(0)' : 'translateX(-100px)',
+            transition: 'transform 1.2s ease-in-out, opacity 1.2s ease-in-out',
+            position: isIntersecting ? 'static' : 'absolute',
+            visibility: isIntersecting ? 'visible' : 'hidden',
+          }}
+        >
           {groupedExperiences.map((page, pageIndex) => (
             <div key={pageIndex} className="flex flex-col mt-8 w-full items-center gap-2 70-laptop:gap-6">
               {page.map((experience, experienceIndex) => {
@@ -230,7 +323,7 @@ export function Experiences() {
         </div>
       )}
 
-      <figure className={`flex justify-center w-full h-fit mt-auto ${imgLaptopSize} ${imgDesktopSize} `}>
+      <figure className={`flex justify-center w-full h-fit mt-auto ${imgLaptopSize} ${imgDesktopSize}`}>
         <img src="programming-3.svg" alt="Programming illustration" className="w-full h-full" />
       </figure>
     </div>
