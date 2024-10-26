c3b91eebd70ff065ddc28fe731c48701b1d1b8ff
Igor <iigorfelipe@gmail.com>
2024-10-26 13:31:46 -0300

    refactor: move mock data to utils for better organization

--- src/sections/experiences.tsx
+++ src/sections/experiences.tsx
@@ -20,157 +20,13 @@ import {
 } from '@/components/ui/dialog';
 import { useIntersectionObserver } from '@/hooks/use-intersection-observer';
 import { TooltipDemo } from '@/components/tooltip';
-
-type Log = {
-  date: string;
-  position: string;
-};
-
-type CompanyDetail = {
-  companyName: string;
-  color: string;
-  about: string[];
-  duration: string;
-  logo: string;
-  roleDetails: string[];
-  log: Log[];
-};
-
-type Experiences = {
-  trybeExperience: CompanyDetail;
-  atlasExperience: CompanyDetail;
-};
-
-const experiences: Experiences = {
-  trybeExperience: {
-    companyName: 'Trybe - Escola de Tecnologia online',
-    color: '#1EB720',
-    about: [
-      `A Trybe é uma escola de tecnologia online fundada em 2019, que oferece um currículo prático, desenvolvido em parceria com empresas do setor. A abordagem da Trybe combina teoria e prática, preparando os alunos com habilidades técnicas essenciais, como desenvolvimento web, análise de dados e ciência da computação.`,
-
-      `Além do conhecimento técnico, a Trybe forma profissionais competentes e prontos para o mercado, enfatizando competências comportamentais como trabalho em equipe, resolução de problemas e adaptabilidade. Com um compromisso com a inclusão e diversidade, a Trybe conecta talentos a oportunidades valiosas, contribuindo para o fortalecimento da indústria de tecnologia.`,
-    ],
-    duration: 'ago de 2021 | ago de 2022',
-    logo: 'https://github.com/user-attachments/assets/9bc146b0-e28f-42b2-bcb8-c69ab3ad829c',
-    roleDetails: [
-      'Aprofundamento nos fundamentos do desenvolvimento web, incluindo Git, Unix, Bash, HTML, CSS e JavaScript.',
-      'Desenvolvimento de aplicações frontend utilizando TypeScript, React, React Hooks e React Router, com ênfase em acessibilidade e usabilidade.',
-      'Implementação de testes unitários e automação de testes com React Testing Library, garantindo a qualidade e robustez do código.',
-      'Desenvolvimento de backend em Node.js, incluindo Programação Orientada a Objetos, construção de APIs REST com Express.js e gerenciamento de banco de dados MySQL utilizando Sequelize.',
-      'Utilização de Docker para contêinerização de aplicações, promovendo um ambiente de desenvolvimento eficiente.',
-      'Participação ativa em projetos em grupo, praticando habilidades interpessoais como pensamento crítico, colaboração e comunicação assertiva.',
-      'Participação em workshops e atividades de soft skills, promovendo liderança e trabalho em equipe em um ambiente dinâmico e desafiador.',
-    ],
-
-    log: [
-      {
-        date: 'ago de 2021',
-        position: 'Fundamentos do Desenvolvimento Web',
-      },
-      {
-        date: '2021',
-        position: 'Desenvolvimento Front-End',
-      },
-      {
-        date: '2021',
-        position: 'Desenvolvimento Back-End',
-      },
-      {
-        date: '2021',
-        position: 'Ciência da Computação',
-      },
-      {
-        date: 'ago de 2022',
-        position: 'Desenvolvimento Web',
-      },
-    ],
-  },
-
-  atlasExperience: {
-    companyName: 'Atlas - Segurança e Inteligência Logística',
-    color: '#FF5618',
-    duration: 'out de 2022 | jun de 2024',
-    logo: 'https://github.com/user-attachments/assets/09513a1b-c465-45ae-bcc1-666bf6523251',
-    about: [
-      `A Atlas atua no mercado de Segurança e Inteligência Logística, reconhecida pelo seu trabalho em
-        proteger operações logísticas por meio de soluções tecnológicas de ponta. Atuando desde 2004, a
-        empresa se destaca por atender centenas de clientes de diversos setores e tamanhos, oferecendo um alto
-        nível de segurança e controle para suas operações.`,
-
-      `Com um compromisso sólido de qualidade, a Atlas adota uma abordagem personalizada, ajustando suas
-        soluções às necessidades específicas de cada cliente. Esse foco em inovação e atendimento diferenciado
-        a posiciona como uma referência no mercado, com parcerias de longo prazo e uma reputação de
-        excelência. Trabalhar na Atlas significa atuar em uma equipe qualificada e apaixonada, empenhada em
-        entregar o melhor em segurança e tecnologia logística.`,
-    ],
-    roleDetails: [
-      'Desenvolvi e mantive aplicações web modernas e dinâmicas, utilizando Next.js, React, TypeScript, Context API, Redux e Material UI para garantir interfaces intuitivas e responsivas.',
-      'Colaborei com designers no Figma para criar experiências de usuário otimizadas, alinhando design e funcionalidade.',
-      'Desenvolvi componentes performáticos, priorizando eficiência e reutilização para otimizar a experiência do usuário e o desempenho da aplicação.',
-      'Documentei minhas atividades de desenvolvimento, mantendo registros claros para facilitar manutenções futuras e o alinhamento da equipe.',
-      'Realizei atualizações e manipulação de dados em bancos de dados como MongoDB e MySQL, garantindo integridade e performance dos dados.',
-      'Implementei microsserviços em Node.js para automatizar tarefas, aprimorando processos internos e aumentando a produtividade da equipe.',
-      'Participei de reuniões diárias de alinhamento, reportando o progresso das atividades e colaborando para o sucesso coletivo do projeto.',
-    ],
-    log: [
-      {
-        date: 'out de 2022',
-        position: 'Desenvolvedor Frontend',
-      },
-      {
-        date: '2024',
-        position: 'Desenvolvedor FullStack',
-      },
-    ],
-  },
-};
-
-type ComapnyIdType = 'trybeExperience' | 'atlasExperience';
-
-type ExperienceType = Log & {
-  companyId: ComapnyIdType;
-};
-
-const page1: ExperienceType[] = [
-  {
-    companyId: 'trybeExperience',
-    date: 'ago de 2021',
-    position: 'Fundamentos do Desenvolvimento Web',
-  },
-  {
-    companyId: 'trybeExperience',
-    date: '2021',
-    position: 'Desenvolvimento Front-End',
-  },
-  {
-    companyId: 'trybeExperience',
-    date: '2021',
-    position: 'Desenvolvimento Back-End',
-  },
-  {
-    companyId: 'trybeExperience',
-    date: '2021',
-    position: 'Ciência da Computação',
-  },
-];
-
-const page2: ExperienceType[] = [
-  {
-    companyId: 'trybeExperience',
-    date: 'ago de 2022',
-    position: 'Desenvolvimento Web',
-  },
-  {
-    companyId: 'atlasExperience',
-    date: 'out de 2022',
-    position: 'Desenvolvedor Frontend',
-  },
-  {
-    companyId: 'atlasExperience',
-    date: '2024',
-    position: 'Desenvolvedor FullStack',
-  },
-];
+import {
+  ComapnyIdType,
+  experiences,
+  ExperienceType,
+  firstPageOfTheCarouselOfExperiences,
+  secondPageOfTheCarouselOfExperiences,
+} from '@/utils/experiences';
 
 type CardProps = {
   experience: ExperienceType;
@@ -258,7 +114,10 @@ function Card({ experience }: CardProps) {
 }
 export function Experiences() {
   const { ref, isIntersecting } = useIntersectionObserver(0.4);
-  const [groupedExperiences, setGroupedExperiences] = useState<ExperienceType[][]>([page1, page2]);
+  const [groupedExperiences, setGroupedExperiences] = useState<ExperienceType[][]>([
+    firstPageOfTheCarouselOfExperiences,
+    secondPageOfTheCarouselOfExperiences,
+  ]);
 
   const isMobile = useMediaQuery('(max-width: 499px)');
   const widthIs896pxDown = useMediaQuery('(max-width: 896px)');
@@ -269,12 +128,17 @@ export function Experiences() {
 
   useEffect(() => {
     if (!widthIs896pxDown) {
-      const filteredPage1 = [...page1, ...page2.filter((exp) => exp.companyId === 'trybeExperience')];
-      const filteredPage2 = page2.filter((exp) => exp.companyId === 'atlasExperience');
+      const filteredPage1 = [
+        ...firstPageOfTheCarouselOfExperiences,
+        ...secondPageOfTheCarouselOfExperiences.filter((exp) => exp.companyId === 'trybeExperience'),
+      ];
+      const filteredPage2 = secondPageOfTheCarouselOfExperiences.filter(
+        (exp) => exp.companyId === 'atlasExperience',
+      );
 
       setGroupedExperiences([filteredPage1, filteredPage2]);
     } else {
-      setGroupedExperiences([page1, page2]);
+      setGroupedExperiences([firstPageOfTheCarouselOfExperiences, secondPageOfTheCarouselOfExperiences]);
     }
   }, [widthIs896pxDown]);
 
--- src/sections/works.tsx
+++ src/sections/works.tsx
@@ -7,47 +7,10 @@ import {
 } from '@/components/ui/carousel';
 import { useIntersectionObserver } from '@/hooks/use-intersection-observer';
 import { useMediaQuery } from '@/hooks/use-media-query';
+import { works } from '@/utils/works';
 import { GitHubLogoIcon } from '@radix-ui/react-icons';
 import { ExternalLinkIcon, GlobeIcon, LaptopIcon } from 'lucide-react';
 import { useState } from 'react';
-
-const works = [
-  {
-    title: 'Tree View',
-    description:
-      'Visualização hierárquica dos ativos de uma empresa, abrangendo componentes, ativos e localizações.',
-    desktopImg: 'https://github.com/user-attachments/assets/48603f1a-9599-4843-9e80-573c7b4d9211',
-    mobileImg: 'https://github.com/user-attachments/assets/757721ee-2606-46a5-aa74-6f1ccfc8ae4d',
-    githubLink: 'https://github.com/iigorfelipe/tree-view',
-    appLink: 'https://iigorfelipe.github.io/tree-view/',
-  },
-  {
-    title: 'Invoice Insight',
-    description:
-      'Listagem performática de clientes e prestações por mês. Inclui gráfico e filtragem por períodos.',
-    desktopImg: 'https://github.com/user-attachments/assets/d4abc541-0723-4db2-9b79-7fb4984dfe95',
-    mobileImg: 'https://github.com/user-attachments/assets/d309ea64-b83b-4403-a22b-c1f294aab791',
-    githubLink: 'https://github.com/iigorfelipe/invoice-insight',
-    appLink: 'https://iigorfelipe.github.io/invoice-insight/',
-  },
-  {
-    title: 'Github Explorer',
-    description:
-      'Explore repositórios GitHub com facilidade. Filtragem por tipo, linguagem e ordenação disponíveis.',
-    desktopImg: 'https://github.com/user-attachments/assets/18690840-5962-440f-b1d2-53bce00dc9cd',
-    mobileImg: 'https://github.com/user-attachments/assets/71850e8b-8c75-4ed6-9fd0-b416580dc045',
-    githubLink: 'https://github.com/iigorfelipe/github-explorer',
-    appLink: 'https://iigorfelipe.github.io/github-explorer/',
-  },
-  {
-    title: 'Woovi',
-    description: 'Guia de Pagamento: Pix e Cartão de Crédito Parcelado no Woovi',
-    desktopImg: 'https://github.com/user-attachments/assets/c6c9119d-e22b-4dde-8450-73c3041fcd69',
-    mobileImg: 'https://github.com/user-attachments/assets/527058fc-cda5-4a84-816d-3bc394572cb5',
-    githubLink: 'https://github.com/iigorfelipe/woovi',
-    appLink: 'https://iigorfelipe.github.io/woovi/',
-  },
-];
 
 type ShowMobileProps = {
   title: string;
